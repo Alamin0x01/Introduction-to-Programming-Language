4aee50d2f36898415ec6c2ab27e8b032cc12671b
Md Alamin Ali <md.alamin.ali.096@gmail.com>
2025-04-07 16:41:12 +0600

    printing-variables

--- 1.Basic syntax, variables and data types/Variables in C/first.c
+++ 1.Basic syntax, variables and data types/Variables in C/first.c
@@ -1,10 +1,20 @@
 #include<stdio.h>
 
-int main ()
+int main()
 {
-    int num1 =5;
-    num1 =20;
-    float f =4.56;
+    int num1 = 5;
+    num1 = 20;
+    float f = 4.56;
     char c = '@';
+
+    printf("%f", f);
+
     return 0;
 }
+
+// formet specifiers
+// %d - integer
+// %f - float
+// %c - character
+// %s - string
+// %lf - double
