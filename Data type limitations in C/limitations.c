7de8835f1b7be5dc413d4ca2317e06d02b819d69
Md Alamin Ali <md.alamin.ali.096@gmail.com>
2025-04-10 18:05:04 +0600

    float to double uses

--- Data type limitations in C/limitations.c
+++ Data type limitations in C/limitations.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 int main()
 {
-    long long int a = 10000000000000;
-    printf("%lld", a); // %lld for long long int
+    // long long int a = 10000000000000;
+    // printf("%lld", a); // %lld for long long int
 
+    double d = 890123.456789;
+    printf("%lf", d);
     return 0;
 }
