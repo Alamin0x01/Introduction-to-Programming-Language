aec48c794139a77059d29c3160e3c94e1c698db0
Md Alamin Ali <md.alamin.ali.096@gmail.com>
2025-04-10 18:26:20 +0600

    Operators

---
+++ 2. Operators, Conditional Statements/Arithmetic Operators/Arithmetic.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a = 50;
+    int b = 20;
+    int sum = a + b;
+    printf("sum= %d\n", sum);
+    int sub = a - b;
+    printf("sub= %d\n", sub);
+    int mul = a * b;
+    printf("mul= %d\n", mul);   
+    int div = a / b;
+    printf("div= %d\n", div);
+    return 0;
+}
