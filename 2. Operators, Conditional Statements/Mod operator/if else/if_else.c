470f90c0fea70a3a66aff64c4800f4ef163a5059
Md Alamin Ali <md.alamin.ali.096@gmail.com>
2025-04-24 00:23:10 +0600

    if else

---
+++ 2. Operators, Conditional Statements/Mod operator/if else/if_else.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int tk;
+    scanf("%d", &tk);
+    if (tk >= 100)
+    {
+        printf("I will buy Punjabi\n");
+    }
+    else
+        printf("Bad luck!\n");
+    return 0;
+}
