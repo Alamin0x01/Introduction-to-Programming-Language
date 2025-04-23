81b7d4738d57907e17dd824633ac2a743621d75c
Md Alamin Ali <md.alamin.ali.096@gmail.com>
2025-04-23 23:48:39 +0600

    relaional operators

---
+++ 2. Operators, Conditional Statements/2-3 Relational operators/Relational-operators.c
@@ -0,0 +1,26 @@
+// #include <stdio.h>
+// #include <string.h>
+// #include <math.h>
+// #include <stdlib.h>
+
+// int main()
+// {
+//     long long N;
+//     scanf("%lld", &N);
+
+//     if (N <= 1000)
+//     {
+//         printf("Bad luck!\n");
+//     }
+//     else
+//     {
+//         printf("I will buy Punjabi\n");
+//         N -= 1000;
+//         if (N >= 500)
+//         {
+//             printf("I will buy new shoes\n");
+//             printf("Alisa will buy new shoes\n");
+//         }
+//     }
+//     return 0;
+// }
