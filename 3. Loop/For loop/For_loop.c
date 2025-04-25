fff4408af83afa713c5045dc3555e59490d7ece5
Md Alamin Ali <md.alamin.ali.096@gmail.com>
2025-04-25 18:53:00 +0600

    for loop

--- 3. Loop/For loop/For_loop.c
+++ 3. Loop/For loop/For_loop.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-int main()
+int main() 
+{
+    // This program prints "Hello World" 10 times using a for loop.
+    // The loop starts at 1 and increments by 1 until it reaches 10.
+    // In each iteration, it prints "Hello World".
+    
+    // For loop to print "Hello World" 10 times
 {
     for (int i = 1; i <= 10; i = i + 1)
     {
