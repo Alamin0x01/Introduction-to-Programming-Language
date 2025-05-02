bedad53b6e5f367ae6ec00174a6bf661afe72d33
Md Alamin Ali <md.alamin.ali.096@gmail.com>
2025-05-03 00:37:52 +0600

    new problem fixed

--- 2. Operators, Conditional Statements/Nested if else/nested_if_else.c
+++ 2. Operators, Conditional Statements/Nested if else/nested_if_else.c
@@ -22,3 +22,27 @@ int main()
 
     return 0;
 }
+
+// #include <stdio.h>
+// #include <string.h>
+
+// int main()
+// {
+//     char s[10001];
+//     scanf("%s", s);
+
+//     int charCount[26] = {0};
+//     for (int i = 0; s[i] != '\0'; i++)
+//     {
+//         charCount[s[i] - 'a']++;
+//     }
+//     for (int i = 0; i < 26; i++)
+//     {
+//         if (charCount[i] > 0)
+//         {
+//             printf("%c - %d\n", 'a' + i, charCount[i]);
+//         }
+//     }
+
+//     return 0;
+// }
